588334c321e7a2b85a49cb8d6fd6c6afe591d64f
Noe Garcia <jnoe.garciar@gmail.com>
2021-01-05 02:15:49 -0600

    Funcional 1 en produccion

--- src/app/views/app/app.module.ts
+++ src/app/views/app/app.module.ts
@@ -10,12 +10,22 @@ import { ProyectsComponent } from './proyects/proyects.component';
 import { AddProyectComponent } from './add-proyect/add-proyect.component';
 import {ArchwizardModule} from "angular-archwizard";
 import {NgSelectModule} from "@ng-select/ng-select";
-import {FormsModule, FormsModule as FormsModuleAngular} from '@angular/forms';
+import {FormsModule, FormsModule as FormsModuleAngular, ReactiveFormsModule} from '@angular/forms';
 import {PaginationModule} from "ngx-bootstrap/pagination";
 import {NgxDatatableModule} from "@swimlane/ngx-datatable";
 import {CollapseModule} from "ngx-bootstrap/collapse";
 import {PagesContainersModule} from "../../containers/pages/pages.containers.module";
 import {ListProductsComponent} from "./products/list-products/list-products.component";
+import {ComponentsCarouselModule} from "../../components/carousel/components.carousel.module";
+import {ComponentsCardsModule} from "../../components/cards/components.cards.module";
+import {ComponentsChartModule} from "../../components/charts/components.charts.module";
+import {RatingModule} from "ngx-bootstrap/rating";
+import {HotkeyModule} from "angular2-hotkeys";
+import {TabsModule} from "ngx-bootstrap/tabs";
+import {ModalModule} from "ngx-bootstrap/modal";
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import {AccordionModule} from "ngx-bootstrap/accordion";
+import {ContextMenuModule} from "ngx-contextmenu";
 
 
 @NgModule({
@@ -35,7 +45,25 @@ import {ListProductsComponent} from "./products/list-products/list-products.comp
     NgxDatatableModule,
     CollapseModule,
     PagesContainersModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    SharedModule,
+    ComponentsCarouselModule,
+    LayoutContainersModule,
+    PagesContainersModule,
+    ComponentsCardsModule,
+    ComponentsChartModule,
+    RatingModule.forRoot(),
+    FormsModuleAngular,
+    ReactiveFormsModule,
+    HotkeyModule.forRoot(),
+    PaginationModule.forRoot(),
+    TabsModule.forRoot(),
+    ModalModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    AccordionModule.forRoot(),
+    ContextMenuModule.forRoot({
+      useBootstrap4: true,
+    })
   ]
 })
 export class AppModule { }
--- src/app/views/app/products/list-products/list-products.component.ts
+++ src/app/views/app/products/list-products/list-products.component.ts
@@ -1,6 +1,10 @@
 import {Component, ViewChild} from '@angular/core';
 import {ColumnMode} from "@swimlane/ngx-datatable";
 import productItems from 'src/app/data/products';
+import {ApiService, IProduct} from "../../../../data/api.service";
+import {Hotkey, HotkeysService} from "angular2-hotkeys";
+import {ContextMenuComponent} from "ngx-contextmenu";
+import {AddNewProductModalComponent} from "../../../../containers/pages/add-new-product-modal/add-new-product-modal.component";
 
 @Component({
   selector: 'app-list-products',
@@ -8,48 +12,120 @@ import productItems from 'src/app/data/products';
   styleUrls: ['./list-products.component.scss']
 })
 export class ListProductsComponent {
-  @ViewChild('myTable') table: any;
-
-  expanded: any = {};
-  timeout: any;
-  rows = productItems.slice(0, 20).map(({ title, sales, stock, category, date }) => ({ title, sales, stock, category, date }));
+  displayMode = 'thumb';
+  selectAllState = '';
+  selected: IProduct[] = [];
+  data: IProduct[] = [];
+  currentPage = 1;
   itemsPerPage = 10;
-  ColumnMode = ColumnMode;
-  columns = [
-    { prop: 'title', name: 'Title' },
-    { prop: 'sales', name: 'Sales' },
-    { prop: 'stock', name: 'Stock' },
-    { prop: 'category', name: 'Category' },
-    { prop: 'date', name: 'Date' }
-  ];
-  temp = [...this.rows];
-  constructor() {
+  search = '';
+  orderBy = '';
+  isLoading: boolean;
+  endOfTheList = false;
+  totalItem = 0;
+  totalPage = 0;
 
+
+  @ViewChild('basicMenu') public basicMenu: ContextMenuComponent;
+  @ViewChild('addNewModalRef', { static: true }) addNewModalRef: AddNewProductModalComponent;
+
+  constructor(private hotkeysService: HotkeysService, private apiService: ApiService) {
+    this.hotkeysService.add(new Hotkey('ctrl+a', (event: KeyboardEvent): boolean => {
+      this.selected = [...this.data];
+      return false;
+    }));
+    this.hotkeysService.add(new Hotkey('ctrl+d', (event: KeyboardEvent): boolean => {
+      this.selected = [];
+      return false;
+    }));
   }
 
-  onPage(event) {
+
+  ngOnInit() {
+    this.loadData(this.itemsPerPage, this.currentPage, this.search, this.orderBy);
   }
 
-  toggleExpandRow(row) {
-    this.table.rowDetail.toggleExpandRow(row);
-  }
+  loadData(pageSize: number = 10, currentPage: number = 1, search: string = '', orderBy: string = '') {
+    this.itemsPerPage = pageSize;
+    this.currentPage = currentPage;
+    this.search = search;
+    this.orderBy = orderBy;
 
-  onDetailToggle(event) {
-  }
-
-  updateFilter(event) {
-    const val = event.target.value.toLowerCase().trim();
-    const count = this.columns.length;
-    const keys = Object.keys(this.temp[0]);
-    const temp = this.temp.filter(item => {
-      for (let i = 0; i < count; i++) {
-        if ((item[keys[i]] && item[keys[i]].toString().toLowerCase().indexOf(val) !== -1) || !val) {
-          return true;
+    this.apiService.getProducts(pageSize, currentPage, search, orderBy).subscribe(
+      data => {
+        if (data.status) {
+          this.isLoading = false;
+          this.data = data.data;
+          this.totalItem = data.totalItem;
+          this.totalPage = data.totalPage;
+          this.setSelectAllState();
+        } else {
+          this.endOfTheList = true;
         }
+      },
+      error => {
+        this.isLoading = false;
       }
-    });
-    this.rows = temp;
-    this.table.offset = 0;
+    );
   }
 
+  changeDisplayMode(mode) {
+    this.displayMode = mode;
+  }
+
+  showAddNewModal() {
+    this.addNewModalRef.show();
+  }
+
+  isSelected(p: IProduct) {
+    return this.selected.findIndex(x => x.id === p.id) > -1;
+  }
+  onSelect(item: IProduct) {
+    if (this.isSelected(item)) {
+      this.selected = this.selected.filter(x => x.id !== item.id);
+    } else {
+      this.selected.push(item);
+    }
+    this.setSelectAllState();
+  }
+
+  setSelectAllState() {
+    if (this.selected.length === this.data.length) {
+      this.selectAllState = 'checked';
+    } else if (this.selected.length !== 0) {
+      this.selectAllState = 'indeterminate';
+    } else {
+      this.selectAllState = '';
+    }
+  }
+
+  selectAllChange($event) {
+    if ($event.target.checked) {
+      this.selected = [...this.data];
+    } else {
+      this.selected = [];
+    }
+    this.setSelectAllState();
+  }
+
+  pageChanged(event: any): void {
+    this.loadData(this.itemsPerPage, event.page, this.search, this.orderBy);
+  }
+
+  itemsPerPageChange(perPage: number) {
+    this.loadData(perPage, 1, this.search, this.orderBy);
+  }
+
+  changeOrderBy(item: any) {
+    this.loadData(this.itemsPerPage, 1, this.search, item.value);
+  }
+
+  searchKeyUp(event){
+    const val = event.target.value.toLowerCase().trim();
+    this.loadData(this.itemsPerPage, 1, val, this.orderBy);
+  }
+
+  onContextMenuClick(action: string, item: IProduct) {
+    console.log('onContextMenuClick -> action :  ', action, ', item.title :', item.title);
+  }
 }
